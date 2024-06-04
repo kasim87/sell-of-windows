5242e90b5f44dc9c43d9fcd46f757158ca92207b
kasim <kasimsarman@gmail.com>
2024-06-04 19:02:13 +0600

    forms calculate window

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,13 @@ import tabs from './modules/tabs'
 import forms from './modules/form'
 
 window.addEventListener('DOMContentLoaded', () => {
+    let modalState = {
+        
+    }
+
     modals()
     tabs('.glazing_slider ', '.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
+    tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', '.do_image_more', 'inline-block')
     forms()
 })
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,15 +1,19 @@
-
 function modals() {
-    function bindModal(triggerSelector, modalSelector, closeSelector) {
+    function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverLay = true) {
         const trigger = document.querySelectorAll(triggerSelector),
               modal = document.querySelector(modalSelector),
-              close = document.querySelector(closeSelector);
+              close = document.querySelector(closeSelector),
+              windows = document.querySelectorAll('[data-modal]');
 
         trigger.forEach(element => {
             element.addEventListener('click', (e) => {
                 if (e.target) {
                     e.preventDefault()
                 }
+
+                windows.forEach(item => {
+                    item.style.display = 'none'
+                })
 
                 modal.style.display = 'block'
                 // document.body,style.overFlow = 'hidden'
@@ -18,16 +22,25 @@ function modals() {
         })
 
         close.addEventListener('click', () => {
+            windows.forEach(item => {
+                item.style.display = 'none'
+            })
+
             modal.style.display = 'none'
             // document.body,style.overFlow = ''
             document.body.classList.remove('modal-open')
         })
 
         modal.addEventListener('click', (e) => {
-            if (e.target === modal)
+            if (e.target === modal && closeClickOverLay) {
+                windows.forEach(item => {
+                    item.style.display = 'none'
+                })
+
                 modal.style.display = 'none'
                 // document.body,style.overFlow = '' 
                 document.body.classList.remove('modal-open')
+            }
         })
     }
 
@@ -40,7 +53,10 @@ function modals() {
 
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close')
     bindModal('.phone_link', '.popup', '.popup .popup_close')
-    showModalByTime('.popup', 60000)
+    bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close')
+    bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false)
+    bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false)
+    showModalByTime('.popup', 90000)
 }
 
 export default modals
